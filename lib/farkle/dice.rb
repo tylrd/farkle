251cc0f1fe8b5d03ca43fb85029bf2e62d35bee2
Taylor Daugherty <mdaugherty6@gmail.com>
2016-03-22 12:19:38 -0400

    add documentation

--- lib/farkle/dice.rb
+++ lib/farkle/dice.rb
@@ -4,16 +4,27 @@ module Farkle
     def_delegators :@dice, :size, :each
     include Enumerable
 
+    # Initializes an instance of the <code>Farkle::Dice</code> object
+    #
+    # @param options [Hash] the options to create a Dice with
+    # @option options [Integer] :num number of dice
+    # @option options [Integer] :sides number of sides on the die
     def initialize(options = {})
       @dice = Array.new(options[:num] || 6) { Die.new(options[:sides]) }
     end
 
+    # Rolls each dice.
+    #
+    # @return [Array<Farkle::Die>] The array of die that have been rolled.
     def throw
-      @dice.each {|die| die.roll }
+      @dice.each { |die| die.roll }
     end
 
+    # Describes if all the dice have been thrown
+    #
+    # @return [Boolean] true if all dice have been thrown
     def thrown?
-      @dice.all? {|die| die.rolled? }
+      @dice.all? { |die| die.rolled? }
     end
 
   end
